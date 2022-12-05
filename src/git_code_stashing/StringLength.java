48ebc7100ca8693a5f3f35368075f79a878f8b8f
Nandini Rajaram <nandinivijayr@gmail.com>
2022-12-05 16:31:18 -0600

    comitting the java files

---
+++ src/git_code_stashing/StringLength.java
@@ -0,0 +1,17 @@
+package git_code_stashing;
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(wordLength("Hello"));
+		
+	}
+	
+	public static int wordLength(String s)
+	{
+		return s.length();		
+	}
+
+}
